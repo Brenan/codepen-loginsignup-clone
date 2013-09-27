70fce824b7f4ae1af36688248446c6373d55e177
Brenan Klain <brenanklain@gmail.com>
2013-09-26 20:37:28 -0600

    Adds functionailty to login and signup forms

--- script.js
+++ script.js
@@ -29,8 +29,52 @@ Reponse Objects:
 */
 
 
-$('document').ready(function() {
+$(document).ready(function() {
 
-	//put code here
+	$("#signupLink").click(function(){
+		$(".login-form").hide();
+		$(".signup-form").show();
+		$(this).addClass("active");
+		$("#loginLink").removeClass("active");
+	});
 
+	$("#loginLink").click(function(){
+		$(".signup-form").hide();
+		$(".login-form").show();
+		$(this).addClass("active");
+		$("#signupLink").removeClass("active");
+	});
+
+// Login Form Functionality
+	$(".login-form button").click(function(){
+		var newUser = {
+			username: $("#login-email-field").val(),
+			password: $("#login-password-field").val()
+		};
+		var loginResponse = codepen.api.login(newUser);
+		if(loginResponse.success == false){
+			alert(loginResponse.error);
+		}
+
+		return false;
+			
+	});
+
+// Signup Form Funcionality
+
+	$(".signup-form button").click(function(){
+		var newSignUp = {
+			name: $("#signup-name-field").val(),
+			email: $("#signup-email-field").val(),
+			username: $("#signup-username-field").val(),
+			password: $("#signup-password-field").val()
+		}
+		var signUpResponse = codepen.api.signup(newSignUp);
+		if(signUpResponse.success == false){
+			alert(signUpResponse.error);
+		} 
+		return false;
+	});
 });
+
+
