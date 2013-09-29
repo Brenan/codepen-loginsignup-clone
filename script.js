cc90a7c92f05e35721985f9f979365716d739140
Brenan Klain <brenanklain@gmail.com>
2013-09-28 20:42:13 -0600

    Adds prototype for user

--- script.js
+++ script.js
@@ -45,30 +45,51 @@ $(document).ready(function() {
 		$("#signupLink").removeClass("active");
 	});
 
+// Set Up the Prototype
+
+codepen.User = {
+	name: null,
+	username: null,
+	email: null,
+	password: null,
+	setLoginInfo: function(username, password){
+		this.username = username;
+		this.password = password;
+	},
+	setSignInInfo: function(name, password, email, username){
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.username = username;
+	}
+};
+
+
 // Login Form Functionality
 	$(".login-form button").click(function(){
-		var newUser = {
-			username: $("#login-email-field").val(),
-			password: $("#login-password-field").val()
-		};
+		var username = $("#login-email-field").val();
+		var password = $("#login-password-field").val();
+		var newUser = Object.create(codepen.User);
+		newUser.setLoginInfo(username,password);
 		var loginResponse = codepen.api.login(newUser);
 		if(loginResponse.success == false){
 			alert(loginResponse.error);
 		}
 
 		return false;
-			
 	});
 
 // Signup Form Funcionality
 
 	$(".signup-form button").click(function(){
-		var newSignUp = {
-			name: $("#signup-name-field").val(),
-			email: $("#signup-email-field").val(),
-			username: $("#signup-username-field").val(),
-			password: $("#signup-password-field").val()
-		}
+			var name= $("#signup-name-field").val();
+			var email= $("#signup-email-field").val();
+			var username= $("#signup-username-field").val();
+			var password= $("#signup-password-field").val();
+
+			var newSignUp = Object.create(codepen.User);
+			newSignUp.setSignInInfo(name,password,email,username);
+		
 		var signUpResponse = codepen.api.signup(newSignUp);
 		if(signUpResponse.success == false){
 			alert(signUpResponse.error);
